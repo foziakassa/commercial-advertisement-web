5b26797e00d7564a03f5f8a5c999342bdae5f818
fozia <foziakassa19@gmail.com>
2025-08-11 14:25:52 +0300

    nave ui completed

--- src/component/nav/page.tsx
+++ src/component/nav/page.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
-          <div className=" flex-shrink-0 flex flex-row">
+          <div className="-ml-5 flex-shrink-0 flex flex-row">
            <Link href="/" className="flex items-center">
               <Image
               src="/image/logo2.png"
