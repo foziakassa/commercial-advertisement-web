01565befb58f16db3b9780f32dba25ade3fecbfd
fozia <foziakassa19@gmail.com>
2025-08-11 14:37:23 +0300

    z index modified

--- src/component/nav/page.tsx
+++ src/component/nav/page.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="bg-white shadow-lg sticky top-0">
+    <nav className="bg-white shadow-lg sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
