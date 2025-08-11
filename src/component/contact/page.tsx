effdc726cece40c29cf16a2ff53c58a3cce60af4
fozia <foziakassa19@gmail.com>
2025-08-11 15:38:41 +0300

    contact page madified

--- src/component/contact/page.tsx
+++ src/component/contact/page.tsx
@@ -51,6 +51,20 @@ const ContactSection: React.FC = () => {
                     Email address
                   </label>
                 </div>
+                 <div className="relative mb-6">
+                  <input
+                    type="subject"
+                    className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary motion-reduce:transition-none"
+                    id="exampleInput92"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="exampleInput92"
+                    className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary motion-reduce:transition-none"
+                  >
+                   subject
+                  </label>
+                </div>
                 <div className="relative mb-6">
                   <textarea
                     id="exampleFormControlTextarea1"
@@ -65,23 +79,11 @@ const ContactSection: React.FC = () => {
                     Message
                   </label>
                 </div>
-                <div className="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
-                  <input
-                    className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none checked:border-primary checked:bg-primary hover:cursor-pointer focus:shadow-none focus:transition-[border-color_0.2s]"
-                    type="checkbox"
-                    id="exampleCheck96"
-                    defaultChecked
-                  />
-                  <label
-                    className="inline-block pl-[0.15rem] hover:cursor-pointer"
-                    htmlFor="exampleCheck96"
-                  >
-                    Send me a copy of this message
-                  </label>
-                </div>
+                
                 <button
                   type="button"
-                  className="mb-6 w-full rounded bg-sky-500 text-white px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal lg:mb-0"
+                  className="mb-6 w-full rounded bg-blue-500 text-white px-6 pt-2.5 pb-2 text-xs 
+                  font-medium uppercase leading-normal lg:mb-0"
                 >
                   Send
                 </button>
@@ -95,7 +97,7 @@ const ContactSection: React.FC = () => {
                 <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                   <div className="flex items-start">
                     <div className="shrink-0">
-                      <div className="inline-block rounded-md bg-sky-200 p-4 text-primary">
+                      <div className="inline-block rounded-md bg-blue-200 p-4 text-primary">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -124,7 +126,7 @@ const ContactSection: React.FC = () => {
                 <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
                   <div className="flex items-start">
                     <div>
-                      <div className="inline-block rounded-md bg-sky-200 p-4 text-primary">
+                      <div className="inline-block rounded-md bg-blue-200 p-4 text-primary">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
