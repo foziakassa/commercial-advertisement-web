a1dc2b73a8bed8f7ee3c58ca706ca75eec22dea1
fozia <foziakassa19@gmail.com>
2025-08-14 15:57:21 +0300

    contact page maodification

--- src/component/contact/page.tsx
+++ src/component/contact/page.tsx
@@ -51,7 +51,7 @@ const ContactSection: React.FC = () => {
                     Email address
                   </label>
                 </div>
-                 <div className="relative mb-6">
+                <div className="relative mb-6">
                   <input
                     type="subject"
                     className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary motion-reduce:transition-none"
@@ -62,7 +62,7 @@ const ContactSection: React.FC = () => {
                     htmlFor="exampleInput92"
                     className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary motion-reduce:transition-none"
                   >
-                   subject
+                    subject
                   </label>
                 </div>
                 <div className="relative mb-6">
@@ -79,7 +79,7 @@ const ContactSection: React.FC = () => {
                     Message
                   </label>
                 </div>
-                
+
                 <button
                   type="button"
                   className="mb-6 w-full rounded bg-blue-500 text-white px-6 pt-2.5 pb-2 text-xs 
@@ -100,24 +100,25 @@ const ContactSection: React.FC = () => {
                       <div className="inline-block rounded-md bg-blue-200 p-4 text-primary">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
                           fill="none"
                           viewBox="0 0 24 24"
-                          strokeWidth={2}
                           stroke="currentColor"
-                          className="h-6 w-6"
                         >
                           <path
+                            d="M3 8.25V21h18V8.25M3 8.25l9 5.25 9-5.25M3 8.25h18M3 8.25v-2.25A2.25 2.25 0 015.25 4h13.5A2.25 2.25 0 0118 6.75v2.25"
+                            strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z"
                           />
                         </svg>
                       </div>
                     </div>
                     <div className="ml-6 grow">
-                      <p className="mb-2 font-bold">Technical support</p>
+                      <p className="mb-2 font-bold">E-Mail</p>
                       <p className="text-sm text-neutral-500">example@gmail.com</p>
-                      <p className="text-sm text-neutral-500">1-600-890-4567</p>
+                      {/* <p className="text-sm text-neutral-500">1-600-890-4567</p> */}
                     </div>
                   </div>
                 </div>
@@ -146,9 +147,9 @@ const ContactSection: React.FC = () => {
                     <div className="ml-6 grow">
                       <p className="mb-2 font-bold">Address</p>
                       <p className="text-sm text-neutral-500">
-                        abcd,
+                       Addis Ababa , Ethiopia 
                         <br />
-                        xyz
+                        22 , xx boulding first floor 
                         <br />
                       </p>
                     </div>
@@ -156,7 +157,7 @@ const ContactSection: React.FC = () => {
                 </div>
 
                 {/* Land Line */}
-                <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:mb-12 lg:w-full lg:px-6 xl:w-6/12">
+                {/* <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:mb-12 lg:w-full lg:px-6 xl:w-6/12">
                   <div className="flex items-start">
                     <div className="shrink-0">
                       <div className="inline-block rounded-md bg-sky-200 p-4 text-primary">
@@ -185,7 +186,7 @@ const ContactSection: React.FC = () => {
                       <p className="text-neutral-500">(0421) 431 2030</p>
                     </div>
                   </div>
-                </div>
+                </div> */}
 
                 {/* Mobile */}
                 <div className="w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:mb-12 xl:w-6/12">
