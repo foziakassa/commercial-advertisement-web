06d029f99750d1d771c1435d84c28a0da246bd82
fozia <foziakassa19@gmail.com>
2025-09-02 08:41:53 +0300

    debuging google console

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://commercial-advertisement-web.vercel.app/sitemap.xml',
+    sitemap: 'https://commercial-advertisement-web.vercel.app/sitemap.ts',
   }
 }
